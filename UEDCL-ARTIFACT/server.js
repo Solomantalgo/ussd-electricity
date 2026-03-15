cb0a22a10b761acf30c79db9f3e490189b6be91a
Kisense Solomon <Kmantalgosolo@gmail.com>
2026-03-15 15:07:28 +0300

    Changed name of bakend to uedcl-artifact

--- UEDCL-ARTIFACT/server.js
+++ UEDCL-ARTIFACT/server.js
@@ -4,15 +4,26 @@ const app = express();
 const port = 3000;
 
 //connect the database
-const db = new sqlite3.Database('./database.sqlite'); (err) => {
+const db = new sqlite3.Database('./database.sqlite', (err) => {
     if (err) {
         console.log(`failed to connect to the database ${err.message}`)
     }
     else {
         console.log(`connected to the database`)
     }
-};
+});
 
+//create the transaction table if not exist
+db.run(`
+    CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Phone_number TEXT NOT NULL,
+        meter_number TEXT NOT NULL,
+        token TEXT NOT NULL,
+        amount INTEGER NOT NULL,
+        status TEXT NOT NULL
+    )
+`)
 
 app.use(express.urlencoded({ extended: false }))
 
@@ -50,7 +61,7 @@ app.post('/ussd', (req, res) => {
 
         //insert into the Database
         db.run(
-            'INSERT INTO transactions (Phone_number, meter_number, token, ammount, status) VALUES (?,?,?,?,?)',
+            'INSERT INTO transactions (Phone_number, meter_number, token, amount, status) VALUES (?,?,?,?,?)',
             [phoneNumber, meterNumber, generatedToken, amount, status],
             function (err) {
                 if (err) {
@@ -84,6 +95,42 @@ app.post('/ussd', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Welcome to the USSD Electricty Backend');
 });
+
+//Route to view all transactions
+app.get('/transactions', (req, res) => {
+    db.all('SELECT * FROM transactions', [], (err, rows) => {
+        if(err){
+            res.status(500).json({error: err.message})
+            return;
+        }
+        res.json(rows);
+    })
+});
+
+//Esp32 route 
+app.get('/api/tokens/:meterNumber', (req, res) => {
+    const meterNumber = req.params.meterNumber;
+
+    //Search the database for the token 
+    db.get(
+        `SELECT token FROM transactions WHERE meter_number = ? AND status = 'Unused' ORDER BY ID ASC LIMIT 1`,
+        [meterNumber],
+        (err, row) => {
+            if(err){
+                res.status(500).json({error:err.message})
+                return;
+            }
+            if(row){
+                //if token is found send it to the Esp32
+                res.json({token:row.token, hasNewToken:true});
+            } else {
+                //if no token found, tell the esp32 to keep waiting
+                res.json({hasNewToken:false})
+            }
+        }
+    )
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
